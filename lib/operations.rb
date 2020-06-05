2321f9378b4ff270b99b2ea734ae8b92c1e7cb64
Jonathan Snipe <jon.mike919@gmail.com>
2020-06-05 21:19:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
+require 'pry'
 
+unsafe? = "60 > 40 || 40 < 16"
+
+def unsafe?(speed)
+  puts TRUE
+    if "unsafe?" == "60 > 40 || 40 < 16"
+    elsif "Unsafe?" == "50"
+    puts FALSE
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
 	
 
